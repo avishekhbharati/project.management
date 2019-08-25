5439ccc17fa94472b6866cd970d375a01916739a
Ren <renchalissery1994@gmail.com>
2019-08-25 18:13:30 +1000

    Added server package and Junit test case sample

--- src/main/java/sef/project/management/controller/UserController.java
+++ src/main/java/sef/project/management/controller/UserController.java
@@ -3,37 +3,32 @@ package sef.project.management.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import sef.project.management.entity.User;
-import sef.project.management.repository.UserRepository;
+import sef.project.management.service.UserService;
 
 @Controller
-@RequestMapping(path="/user")
+@ResponseBody
+@RequestMapping(path = "/user")
 public class UserController {
-	
+
 	@Autowired
-	private UserRepository userRepository;
+	private UserService userService;
 
-	@GetMapping(path="/")
-	public @ResponseBody String test () {
-		return "Hello World!";
-	}
-	
-	@GetMapping(path="/save")
-	public @ResponseBody String addNewUser (@RequestParam String name
-			, @RequestParam String email) {
-
-		User n = new User();
-		n.setName(name);
-		n.setEmail(email);
-		userRepository.save(n);
-		return "Saved";
+	@GetMapping(path = "/list")
+	public List<User> getAllUsers() {
+		return userService.getAllUsers();
 	}
 
-	@GetMapping(path="/list")
-	public @ResponseBody List<User> getAllUsers() {
-		return userRepository.getUsers();
+	@GetMapping(path = "/save")
+	public User addNewUser(@RequestParam String name, @RequestParam String email) {
+		return userService.addNewUser(name, email);
 	}
+
 }
