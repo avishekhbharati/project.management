953fb24092965726eb7ba3b2972b9afa391a3e7a
avishekhbharati <renchalissery1994@gmail.com>
2019-08-26 16:23:04 +1000

    added activity

--- src/main/java/sef/project/management/service/ProjectManagerService.java
+++ src/main/java/sef/project/management/service/ProjectManagerService.java
@@ -1,12 +1,40 @@
 package sef.project.management.service;
 
+import java.sql.Timestamp;
+
 import org.springframework.stereotype.Service;
+
+import sef.project.management.entity.Activity;
+import sef.project.management.entity.User;
 
 @Service
 public class ProjectManagerService {
 
-	public String sampleTest() {
-		return "Test";
+	public Activity scheduleActivity(Activity activity, Timestamp startDate, Timestamp endDate) {
+		if (activity != null && startDate != null && endDate != null) {
+			if (endDate.getTime() > startDate.getTime()) {
+				activity.setStartDate(startDate);
+				activity.setEndDate(endDate);
+			}
+		}
+		return activity;
+	}
+
+	public Activity setActivityDependency(Activity activity, Integer dependencyActivityId) {
+		if (activity != null && activity.getActivityId() != dependencyActivityId) {
+			activity.setDependencyActivityId(dependencyActivityId);
+		}
+		return activity;
 	}
 	
+	public boolean allocateUserToActivity(Activity activity, User user, Timestamp startDate, Timestamp endDate) {
+		boolean isAllocated = false;
+//		if(activity !=null && user.) {
+//			activity.setUser(user);
+//			activity.setStartDate(startDate);
+//			activity.setEndDate(endDate);
+//		}
+		return isAllocated;
+	}
+
 }
--- src/test/java/sef/project/management/ProjectManagerTest.java
+++ src/test/java/sef/project/management/ProjectManagerTest.java
@@ -2,22 +2,44 @@ package sef.project.management;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.sql.Timestamp;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import sef.project.management.entity.Activity;
 import sef.project.management.service.ProjectManagerService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectManagerTest {
 
+	private Activity activity;
 	@Autowired
 	private ProjectManagerService projectManagerService;
 
+	@Before
+	public void setValues() {
+		activity = new Activity();
+		activity.setActivityId(100);
+	}
+
 	@Test
-	public void sampleTest() {
-		assertEquals(projectManagerService.sampleTest(), "Test");
+	public void testScheduleActivity() {
+		Activity activity = projectManagerService.scheduleActivity(this.activity, new Timestamp(100),
+				new Timestamp(200));
+		assertEquals(activity.getStartDate().getTime(), 100);
+		assertEquals(activity.getEndDate().getTime(), 200);
+	}
+
+	@Test
+	public void testDependencyAllocation() {
+		Activity activity = projectManagerService.setActivityDependency(this.activity, 101);
+		Integer dependencyId = activity.getDependencyActivityId();
+		assertEquals(Integer.parseInt(dependencyId.toString()), 101);
 	}
 }
